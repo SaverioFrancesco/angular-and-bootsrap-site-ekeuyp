eaa80cb9600ceaa1381a3a73423281357eb32183
SaverioFrancesco <mitramdhir@gmail.com>
2019-08-20 14:07:31 +0200

    2 files modified, 6 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { TopTabComponent } from './top-tab/top-tab.component';
+import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, TopTabComponent, SliderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
---
+++ src/app/top-tab/top-tab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-top-tab',
+  templateUrl: './top-tab.component.html',
+  styleUrls: ['./top-tab.component.css']
+})
+export class TopTabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
